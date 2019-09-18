37915820f113c60109152970c6f55589f422341f
杨一凡凡 <1020106114@qq.com>
2019-09-18 21:26:31 +0800

    3.0

--- src/main/java/com/baizhi/GittestApplication.java
+++ src/main/java/com/baizhi/GittestApplication.java
@@ -9,6 +9,7 @@ public class GittestApplication {
     public static void main(String[] args) {
         SpringApplication.run(GittestApplication.class, args);
         System.out.println(111);
+        System.out.println(222);
     }
 
 }
